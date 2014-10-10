12f86dd49b8ca6761a44b3e842f699c25ca96a95
Alan Russian <alan@alanrussian.com>
2014-10-10 17:09:45 -0500

    Create location manager.

---
+++ src/org/iwuacm/iwuglasstour/LocationManager.java
@@ -0,0 +1,43 @@
+package org.iwuacm.iwuglasstour;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.iwuacm.iwuglasstour.model.Building;
+
+/**
+ * Keeps track of the user's location and provides access to nearby buildings.
+ */
+public class LocationManager {
+
+	public interface Listener {
+		/**
+		 * Called when the building in front or to the sides have changed.
+		 */
+		void onNearbyBuildingsChange(Building left, Building front, Building right);
+		
+		/**
+		 * Called when the user enters a building.
+		 */
+		void onEnterBuilding(Building building);
+		
+		/**
+		 * Called when the user leaves a building.
+		 */
+		void onExitBuilding();
+	}
+	
+	// TODO: Use.
+	@SuppressWarnings("unused")
+	private final LocationManager locationManager;
+	private final Set<Listener> listeners;
+	
+	public LocationManager(LocationManager locationManager) {
+		this.locationManager = locationManager;
+		this.listeners = new LinkedHashSet<Listener>();
+	}
+	
+	public void addListener(Listener listener) {
+		listeners.add(listener);
+	}
+}
