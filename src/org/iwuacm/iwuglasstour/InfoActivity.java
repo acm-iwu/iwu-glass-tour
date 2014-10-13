4d391da76c793b9b16f8145bffc47e287acbb730
Alan Russian <alan@alanrussian.com>
2014-10-13 16:12:55 -0500

    Create info activity. Add to manifest.

---
+++ src/org/iwuacm/iwuglasstour/InfoActivity.java
@@ -0,0 +1,28 @@
+package org.iwuacm.iwuglasstour;
+
+import org.iwuacm.iwuglasstour.model.Building;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Activity that contains the {@link InfoView}. Provides detailed information about a
+ * {@link Building}.
+ */
+public class InfoActivity extends Activity {
+	
+	/**
+	 * Key for the {@link Intent}'s building extra.
+	 */
+	public static final String BUILDING_MODEL = "buildingModel";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		Building building = (Building) getIntent().getSerializableExtra(BUILDING_MODEL);
+
+		InfoView infoView = new InfoView(building, getApplicationContext());
+		setContentView(infoView);
+	}
+}
