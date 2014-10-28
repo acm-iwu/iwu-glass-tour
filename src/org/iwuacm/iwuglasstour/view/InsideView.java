3ead4625e872387fd723b6ea174fc952ddb59771
Alan Russian <alan@alanrussian.com>
2014-10-27 20:28:40 -0500

    Fix bug where InsideView wasn't rendering.
    This took like 4 hours to figure out, and I still don't know why it
    works. #programmerproblems

--- src/org/iwuacm/iwuglasstour/view/InsideView.java
+++ src/org/iwuacm/iwuglasstour/view/InsideView.java
@@ -1,5 +1,6 @@
 package org.iwuacm.iwuglasstour.view;
 
+import org.iwuacm.iwuglasstour.R;
 import org.iwuacm.iwuglasstour.model.Building;
 import org.iwuacm.iwuglasstour.view.common.CardBuilders;
 
@@ -7,6 +8,8 @@ import com.google.android.glass.widget.CardBuilder;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.FrameLayout;
 
 /**
@@ -28,20 +31,44 @@ public class InsideView extends FrameLayout {
 
     public InsideView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        
+        LayoutInflater.from(context).inflate(R.layout.inside, this);
+
         this.context = context;
     }
     
-    public void setBuilding(Building building) {
+    public void setBuilding(final Building building) {
     	removeAllViews();
+
     	CardBuilder cardBuilder = CardBuilders.createBuildingDescriptionCard(building, context);
     	addView(cardBuilder.getView());
-    	if (listener != null)
-    		listener.onChange();
-    	
+
+    	redoLayout();
+
+    	notifyChange();
     }
 
 	public void setListener(ViewChangeListener listener) {
 		this.listener = listener;
+	}
+	
+	/**
+	 * The layout was not refreshed when {@link setBuilding} was called, and eventually I discovered
+	 * adding this function would fix it. It has something to do with the fact we are adding an item
+	 * to the view after it was initialized. Hopefully I've made my unfamiliarity of Android obvious
+	 * by this comment. ;-) 
+	 */
+	private void redoLayout() {
+		int measuredWidth = View.MeasureSpec.makeMeasureSpec(getWidth(), View.MeasureSpec.EXACTLY);
+		int measuredHeight =
+				View.MeasureSpec.makeMeasureSpec(getHeight(), View.MeasureSpec.EXACTLY);
+
+    	measure(measuredWidth, measuredHeight);
+    	layout(0, 0, getMeasuredWidth(), getMeasuredHeight());
+	}
+	
+	private void notifyChange() {
+		if (listener != null) {
+			listener.onChange();
+		}
 	}
 }
